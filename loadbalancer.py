262e17984932573e8ce1dda3e2cfe03cb80ba52e
Chayan Pathak <chayanpathak2013@gmail.com>
2024-05-29 16:43:26 +0530

    Added various changes to loadbalancer class

--- loadbalancer.py
+++ loadbalancer.py
@@ -1,14 +1,17 @@
 import socket
 import random
+
+#TODO: add timeout support
 class LoadBalancer:
     
-    def __init__(self, serverpool=None):
+    def __init__(self, server_pool=None, load_balancing_strategy=None):
         # Create a new socket which will listen to connections coming from clients.
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.port = 1000
         self.hostname = "127.0.0.1"
-        self.add_server()
-        self.serverpool = serverpool
+        self.server_pool = server_pool
+        self.load_balancing_strategy = load_balancing_strategy
+        self.start(server_pool)
 
     def start(self, ServerPool):
         # Bind the socket to the port and hostname specified in the init function
@@ -28,11 +31,36 @@ class LoadBalancer:
             print(data)
             #if the client has timed out then close the connection
             if not data:
+                # Close the connection with the client
+                self.conn.close()
                 break
-            # # Send a response to the client
-            # self.conn.send(data)
-            # Close the connection with the client
-            self.conn.close()
+            else:
+                # Use load balancing strategy to select the server
+                server = self.load_balancing_strategy.select_server(self.server_pool)
+                server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                try:
+                    # Connect to the server
+                    server_socket.connect((server.hostname, server.port))
+                    # if could not connect to the server then throw exception
+                    # send the request data to the server
+                    server_socket.send(data)
+                    # Receive a response from the server
+                    response = server_socket.recv(1024)
+                    # Send the response to the client
+                    self.conn.send(response)   
+                except:
+                    if server_socket.timeout is not None:
+                        print("Server socket has timedout..... please retry.")
+                    elif server_socket.error is not None:
+                        print("Server is sending error ..... perhaps no server is listening")
+                    else:
+                        print("Unknown error")
+                finally:
+                     # Close the connection with the server
+                    server_socket.close()
+                    # Close the connection with the client
+                    self.conn.close()
+            
 
 
 class LoadBalancingStrategy:
@@ -57,20 +85,20 @@ class ServerPool:
         # Initialize the server pool selection strategy which returns
         # a specified server from the list of servers
         self.strategy = strategy
+        self.health_checker = HealthChecker()
     
     def add_server(self, server):
         self.server_list.append(server)
 
-    def remove_server(self, server, HealthCheck):
+    def remove_server(self, server):
         # Remove the server from the pool if it is not working
         for server in self.server_list:
-            if HealthCheck.is_down(server):
+            if self.health_checker.is_down(server):
                 self.server_pool.remove(server)
 
 class HealthChecker:
     
-    def __init__(self,server_pool) -> None:
-        self.server_pool = server_pool
+    def __init__(self) -> None:
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.data_packet = b'This is a test data packet'
 
@@ -79,16 +107,12 @@ class HealthChecker:
         try:
             # Connect to the server's host and port names as provided
             self.socket.connect(server.hostname, server.port)
-            # Send a dummy packet to the server
-            self.socket.send(self.data_packet)
-            response = self.socket.recv(1024)
-            if response.__contains__("200"):
-            ## pretty bad way to check if the server is down
-            ##trying to check if ok is returned in the payload somewhere has 200
-                return True
-            else:
+            # Send a test data packet to the server
+            # if able to send the test packet that means the server is up and running
+            if self.socket.send(b'This is a test data packet') is not None:
                 return False
-        except:
+            # if socket connection not established then that means the server is down
+        except socket.error:
             return True
 
         
@@ -99,8 +123,14 @@ class Listner:
     pass
 
 class Server:
-    def __init__(self) -> None:
-        self
+    def __init__(self, host_name, port_address) -> None:
+        self.host_name = host_name
+        self.port_address = port_address
+        self.start_listen()
+    
+    def start_listen(self):
+        # Create a new socket which will listen to connections coming from clients.
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 class Client:
     pass
